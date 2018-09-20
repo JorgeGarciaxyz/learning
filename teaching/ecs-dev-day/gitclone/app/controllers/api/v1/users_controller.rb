e95c1087f067b6c9892aaac42b51ea353fad44fd
Jorge García <hegg777@gmail.com>
2018-09-20 17:07:24 -0500

    Teaching[ecs-dev-day]: Add Users create, update and destroy

--- teaching/ecs-dev-day/gitclone/app/controllers/api/v1/users_controller.rb
+++ teaching/ecs-dev-day/gitclone/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,38 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    render json: user
+  end
 
-    render json: @user
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: 201
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+  def update
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors, status: 422
+    end
+  end
+
+  def destroy
+    user.destroy
+
+    render json: user
+  end
+
+  private
+  def user
+    @user ||= User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
