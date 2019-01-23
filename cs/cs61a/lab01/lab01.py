61fed23516b773aedb400dcc59bd9719943c3f8a
Jorge García <hegg777@gmail.com>
2019-01-22 21:18:45 -0600

    CS61A: lab01

---
+++ cs/cs61a/lab01/lab01.py
@@ -0,0 +1,85 @@
+"""Lab 1: Expressions and Control Structures"""
+
+# Coding Practice
+
+def repeated(f, n, x):
+    """Returns the result of composing f n times on x.
+
+    >>> def square(x):
+    ...     return x * x
+    ...
+    >>> repeated(square, 2, 3)  # square(square(3)), or 3 ** 4
+    81
+    >>> repeated(square, 1, 4)  # square(4)
+    16
+    >>> repeated(square, 6, 2)  # big number
+    18446744073709551616
+    >>> def opposite(b):
+    ...     return not b
+    ...
+    >>> repeated(opposite, 4, True)
+    True
+    >>> repeated(opposite, 5, True)
+    False
+    >>> repeated(opposite, 631, 1)
+    False
+    >>> repeated(opposite, 3, 0)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    loop = 1
+    result = x
+    while loop <= n:
+        loop += 1
+        result = f(result)
+    return result
+
+def sum_digits(n):
+    """Sum all the digits of n.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    """
+    "*** YOUR CODE HERE ***"
+    aux = n
+    result = 0
+
+    while aux != 0:
+        result = result + aux % 10
+        aux = aux // 10
+
+    return result
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    aux = n
+    result = False
+    prev_number = 0
+
+    while aux != 0:
+        current_number = aux % 10
+        if current_number == 8 and prev_number == 8:
+            result = True
+            break
+        else:
+            prev_number = current_number
+            aux = aux // 10
+    return result
