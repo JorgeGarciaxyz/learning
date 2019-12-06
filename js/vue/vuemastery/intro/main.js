25a89d003890f1f902e5c5bf5fe58616b61e34f4
Jorge Garcia <jorge.mgg94@gmail.com>
2019-12-05 20:31:40 -0600

    Learning[vue]: Update Vuemastery init course up to class 5

--- js/vue/vuemastery/intro/main.js
+++ js/vue/vuemastery/intro/main.js
@@ -1,7 +1,33 @@
 var app = new Vue({
   el: '#app',
   data: {
+    altText: "A pair of socks",
+    cart: 0,
+    description: 'Some nice socks!',
+    details: ["80% cotton", "20% skill", "Power of will"],
+    image: "./assets/vmSocks-green.jpg",
+    inStock: true,
+    inventory: 10,
     product: 'Socks',
-    description: 'Some nice socks!'
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.jpg"
+      }
+    ]
+  },
+  methods: {
+    changeCartValue(val) {
+      this.cart += val;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 })
