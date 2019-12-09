ab1b180133397457bf4d04550b9a5f38b4660a2c
Jorge Garcia <jorge.mgg94@gmail.com>
2019-12-08 18:45:20 -0600

    Learning[vue]: VueMastery class 7 Computed properties

--- js/vue/vuemastery/intro/main.js
+++ js/vue/vuemastery/intro/main.js
@@ -2,31 +2,44 @@ var app = new Vue({
   el: '#app',
   data: {
     altText: "A pair of socks",
+    brand: "Vue Mastery",
     cart: 0,
     details: ["80% cotton", "20% skill", "Power of will"],
-    image: "./assets/vmSocks-green.jpg",
-    inStock: true,
     inventory: 10,
     product: 'Socks',
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/vmSocks-green.jpg"
+        variantImage: "./assets/vmSocks-green.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue.jpg"
+        variantImage: "./assets/vmSocks-blue.jpg",
+        variantQuantity: 0
       }
-    ]
+    ],
+    selectedVariant: 0
+  },
+  computed: {
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    title() {
+      return this.brand + ' ' + this.product;
+    }
   },
   methods: {
     changeCartValue(val) {
       this.cart += val;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index
     }
   }
 })
