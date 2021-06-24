abbb502a891489847b558354938b349efae86c34
Jorge Garcia <jorge.mgg94@gmail.com>
2021-06-23 22:24:20 -0500

    React: Up to page 29, split main component into list and object components

--- react/eventlite/app/javascript/packs/Eventlite.js
+++ react/eventlite/app/javascript/packs/Eventlite.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import EventsList from './EventsList'
+
 const Eventlite = props => (
   <div>
-    {props.events.map(function(event){
-      return(
-        <div className="event">{event.title}</div>
-      )
-    })}
+    <EventsList events={props.events} />
   </div>
 )
 
