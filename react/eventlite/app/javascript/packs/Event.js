fd746353ddf29535a3d04d2113fcdc325ed683bd
Jorge Garcia <jorge.mgg94@gmail.com>
2021-07-09 16:33:06 -0500

    React[eventlite]: Use Event objects instead of mock data

--- react/eventlite/app/javascript/packs/Event.js
+++ react/eventlite/app/javascript/packs/Event.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+const formatDate = datetime =>
+  new Date(datetime).toDateString()
+
 const Event = props => (
-  <div className="event">{props.event.title}</div>
+  <div className="event">
+    <h2 className="event-title">{props.event.title}</h2>
+    <div className="event-datetime">{formatDate(props.event.start_datetime)}</div>
+    <div className="event-location">{props.event.location}</div>
+  </div>
 )
 
 export default Event
