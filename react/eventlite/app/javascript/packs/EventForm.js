d538c1dc9d6514a2f83bd4a25df7b34df222c170
Jorge Garcia <jorge.mgg94@gmail.com>
2021-10-23 18:18:56 -0500

    React[eventlite]: Use arrow function on the onHandle method to preserve the scope.

--- react/eventlite/app/javascript/packs/EventForm.js
+++ react/eventlite/app/javascript/packs/EventForm.js
@@ -4,15 +4,16 @@ class EventForm extends React.Component {
   constructor(props) {
     super(props)
 
-    this.sate = {
+    this.state = {
       title: '',
       start_datetime: '',
       location: ''
     }
-    this.handleInput = this.handleInput.bind(this);
   }
 
-  handleInput(e) {
+  // Arrow functions use Lexical binding, this function will automatically bind to the
+  // component instance
+  handleInput = (e) => {
     const name = e.target.name;
     const newState = {};
 
@@ -27,9 +28,9 @@ class EventForm extends React.Component {
       <div>
         <h4>Create an Event: </h4>
         <form>
-          <input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleInput} />
+          <input type="text" name="title" placeholder="Title" value={this.state.title}onChange={this.handleInput}/>
           <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime} onChange={this.handleInput}/>
-          <input type="text" name="location" placeholder="Location" value={this.state.Location} onChange={this.handleInput}/>
+          <input type="text" name="location" placeholder="Location" value={this.state.location} onChange={this.handleInput}/>
           <button type="submit">Create Event</button>
         </form>
       </div>
