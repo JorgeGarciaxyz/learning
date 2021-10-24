f43317bc1e4f31a6aac138b1bad6f5f37bb7aaa7
Jorge Garcia <jorge.mgg94@gmail.com>
2021-10-24 17:27:38 -0500

    React: Add functionality to create Event

--- react/eventlite/app/javascript/packs/EventForm.js
+++ react/eventlite/app/javascript/packs/EventForm.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 class EventForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      title: '',
-      start_datetime: '',
-      location: ''
-    }
+      title: "",
+      start_datetime: "",
+      location: "",
+    };
   }
 
   // Arrow functions use Lexical binding, this function will automatically bind to the
@@ -19,23 +20,59 @@ class EventForm extends React.Component {
 
     newState[name] = e.target.value;
 
-    this.setState(newState)
+    this.setState(newState);
     e.preventDefault();
-  }
+  };
 
-  render () {
+  handleSubmit = (e) => {
+    axios({
+      method: "POST",
+      url: "/events",
+      data: { event: this.state },
+      headers: {
+        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    e.preventDefault();
+  };
+
+  render() {
     return (
       <div>
         <h4>Create an Event: </h4>
-        <form>
-          <input type="text" name="title" placeholder="Title" value={this.state.title}onChange={this.handleInput}/>
-          <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime} onChange={this.handleInput}/>
-          <input type="text" name="location" placeholder="Location" value={this.state.location} onChange={this.handleInput}/>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.handleInput}
+          />
+          <input
+            type="text"
+            name="start_datetime"
+            placeholder="Date"
+            value={this.state.start_datetime}
+            onChange={this.handleInput}
+          />
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            value={this.state.location}
+            onChange={this.handleInput}
+          />
           <button type="submit">Create Event</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default EventForm
+export default EventForm;
