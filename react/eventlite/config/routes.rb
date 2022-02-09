f5e1e834fdaee91c8d969ecf045a6ca79210e6b2
Jorge Garcia <jorge.mgg94@gmail.com>
2022-02-08 22:04:58 -0600

    React: create events API endpoint

--- react/eventlite/config/routes.rb
+++ react/eventlite/config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events, only: [:index, :create]
+
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+      resources :events, only: [:index, :create]
+    end
+  end
 end
