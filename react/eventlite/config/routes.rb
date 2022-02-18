1b4ec8fd46afefb3444c67e447890fb0e972d561
Jorge Garcia <jorge.mgg94@gmail.com>
2022-02-18 16:53:49 -0600

    React: Install devise up to page 101

--- react/eventlite/config/routes.rb
+++ react/eventlite/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "events#index"
-
   resources :events, only: [:index, :create]
 
   namespace :api do
     namespace :v1, defaults: { format: :json } do
+      mount_devise_token_auth_for 'User', at: 'auth'
+
       resources :events, only: [:index, :create]
     end
   end
