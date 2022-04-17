157929ebde0620bce9fba028c331b3aa3b4b26fd
Jorge Garcia <jorge.mgg94@gmail.com>
2022-04-16 20:46:31 -0500

    React: change Eventlite to function component

--- react/eventlite-frontend/src/components/Eventlite.js
+++ react/eventlite-frontend/src/components/Eventlite.js
@@ -1,58 +1,38 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
-
 import EventForm from "./EventForm";
 import EventsList from "./EventsList";
-
-import validations from "../validations";
-
 import'./Eventlite.css'
 
-class Eventlite extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      events: []
-    }
-  }
+function Eventlite(props) {
+  const currentUser = localStorage.getItem("user");
+  const [events, setEvents] = useState([]);
 
-  componentDidMount() {
+  useEffect(() =>{
     axios({
       method: 'GET',
       url: 'http://localhost:3001/api/v1/events'
     }).then(response => {
-      this.setState({events: response.data})
+      setEvents(response.data)
     })
-  }
+  }, [])
 
-
-  addNewEvent = (event) => {
-    const events = [event, ...this.state.events].sort(function (a, b) {
+  const addNewEvent = (event) => {
+    const eventsOrdered = [event, ...events].sort(function (a, b) {
       return new Date(a.start_datetime) - new Date(b.start_datetime);
     });
-
-    this.setState({ events: events }, this.changeLogoColour);
+    setEvents(eventsOrdered)
   };
 
-  changeLogoColour = () => {
-    const colors = ["red", "blue", "green", "violet"];
-    this.logo.current.style.color = colors[Math.floor(Math.random() * colors.length)]
-  };
+  return (
+    <div>
+      {currentUser &&
+        <EventForm onSuccess={addNewEvent} />
+      }
 
-  render() {
-    const currentUser = localStorage.getItem("user");
-
-    return (
-      <div>
-        {currentUser &&
-          <EventForm onSuccess={this.addNewEvent} />
-        }
-
-        <EventsList events={this.state.events} />
-      </div>
-    );
-  }
+      <EventsList events={events} />
+    </div>
+  )
 }
 
-export default Eventlite;
+export default Eventlite
