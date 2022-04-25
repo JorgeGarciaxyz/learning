b0ab0c6d6c8d864bf4b65211b5e47ba5b7f82fea
Jorge Garcia <jorge.mgg94@gmail.com>
2022-04-24 20:58:40 -0500

    React: testing using getByRole and Snaptshots

--- react/eventlite-frontend/src/__tests__/EventSummary.test.js
+++ react/eventlite-frontend/src/__tests__/EventSummary.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import EventSummary from '../components/EventSummary';
 import { BrowserRouter as Router } from 'react-router-dom'
+import TestRenderer from 'react-test-renderer';
+
+const event = {
+  title: "My title",
+  start_datetime: "10-10-2022",
+  location: "my house"
+}
 
 it('renders event title', () => {
-  const event = {
-    title: "My title",
-    start_datetime: "10-10-2022",
-    location: "my house"
-  }
-
   render(
   <Router>
     <EventSummary event={event} />
@@ -17,4 +18,24 @@ it('renders event title', () => {
 
   const titleElement = screen.getByText(event.title);
   expect(titleElement).toBeInTheDocument();
+  // If there's an image
+  // const image = getByRole('img', {src: 'event.png'})
+})
+
+it('should render an event summary card', () => {
+  //render(<EventSummary />)
+  render(
+    <Router>
+      <EventSummary event={event} />
+    </Router>
+    )
+  expect(document.body.innerHTML).toMatchSnapshot()
+})
+
+// Not working IDK why
+it('should render an event summary card renderer', () => {
+  //render(<EventSummary />)
+  const tree = TestRenderer.create(<EventSummary event={event} />).toJSON();
+
+  expect(tree).toMatchSnapshot();
 })
